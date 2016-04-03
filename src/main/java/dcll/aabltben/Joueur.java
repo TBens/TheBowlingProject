e07d0d98848a471063d2fc2a00ac23bd6c68c8ab
Thomas BENABENT <TBens>
2016-04-03 18:47:04 +0200

    #20 18  Modification classe Frame et ajout de la gestion des Stikes et Spares

--- src/main/java/dcll/aabltben/Joueur.java
+++ src/main/java/dcll/aabltben/Joueur.java
@@ -43,7 +43,6 @@ public class Joueur {
                 if(secondNextFrameIndx < listeFrames.size()) {
                     scoreFrame += listeFrames.get(secondNextFrameIndx).getPremier();
                 }
-
             } else {
                 scoreFrame += nextFrame.getPremier() + nextFrame.getSecond();
             }
